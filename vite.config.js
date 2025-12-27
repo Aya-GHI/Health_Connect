25630ca5514e0c9ec72cfb5b9eda541c1a7b0066
hanine157 <hanine.hamrouni@supcom.tn>
2025-12-27 14:20:28 +0100

    login_dashboard

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // tailwind.config.js
-import { defineConfig } from 'vite';
 
-export default {
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ['Playfair Display', 'serif'], // headings
-        sans: ['Inter', 'sans-serif'],          // body text
-      },
-    },
-  },
-  plugins: [],
-};
+
+export default defineConfig({
+  plugins: [react()],
+})
+
 
